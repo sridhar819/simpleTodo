7fad72a0c17f5217d76c7c790182b4b24164050b
sridhar819 <srithar1666@gmail.com>
2025-03-27 09:19:04 +0000

    v2

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,66 +1,122 @@
-import './index.css'
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 import TodoItem from '../TodoItem'
 
+import './index.css'
+
 const initialTodosList = [
-  {
-    id: 1,
-    title: 'Book the ticket for today evening',
-  },
-  {
-    id: 2,
-    title: 'Rent the movie for tomorrow movie night',
-  },
-  {
-    id: 3,
-    title: 'Confirm the slot for the yoga session tomorrow morning',
-  },
-  {
-    id: 4,
-    title: 'Drop the parcel at Bloomingdale',
-  },
-  {
-    id: 5,
-    title: 'Order fruits on Big Basket',
-  },
-  {
-    id: 6,
-    title: 'Fix the production issue',
-  },
-  {
-    id: 7,
-    title: 'Confirm my slot for Saturday Night',
-  },
-  {
-    id: 8,
-    title: 'Get essentials for Sunday car wash',
-  },
+  {id: 1, title: 'Book the ticket for today evening'},
+  {id: 2, title: 'Rent the movie for tomorrow movie night'},
+  {id: 3, title: 'Confirm the slot for the yoga session tomorrow morning'},
+  {id: 4, title: 'Drop the parcel at Bloomingdale'},
+  {id: 5, title: 'Order fruits on Big Basket'},
+  {id: 6, title: 'Fix the production issue'},
+  {id: 7, title: 'Confirm my slot for Saturday Night'},
+  {id: 8, title: 'Get essentials for Sunday car wash'},
 ]
 
-// Write your code here
-
 class SimpleTodos extends Component {
-  state = {todolist: initialTodosList}
+  state = {todolist: initialTodosList, topic: ''}
 
   deleteUser = id => {
-    const {todolist} = this.state
-
-    this.setState({
-      todolist: todolist.filter(each => each.id !== id),
-    })
+    this.setState(prevState => ({
+      todolist: prevState.todolist.filter(each => each.id !== id),
+    }))
   }
 
-  render() {
+  checkTodo = (e, id) => {
+    const {checked} = e.target
+
+    this.setState(pre => ({
+      todolist: pre.todolist.map(each =>
+        each.id === id ? {...each, isChecked: checked} : each,
+      ),
+    }))
+  }
+
+  editTodo = id => {
+    this.setState(pre => ({
+      todolist: pre.todolist.map(each =>
+        each.id === id ? {...each, isEdit: true} : each,
+      ),
+    }))
+  }
+
+  saveTodo = (id, topic) => {
+    this.setState(pre => ({
+      todolist: pre.todolist.map(each =>
+        each.id === id ? {...each, title: topic, isEdit: false} : each,
+      ),
+    }))
+  }
+
+  handleTitle = e => {
+    this.setState({topic: e.target.value})
+  }
+
+  onSubmitForm = e => {
+    e.preventDefault()
+    const {topic} = this.state
+
+    const words = topic.split(' ')
+    const numTodos = parseInt(words[words.length - 1], 10)
+
+    const task = words.slice(0, -1).join(' ')
+
+    if (task && typeof numTodos === 'number' && numTodos > 0) {
+      const newTodos = Array.from({length: numTodos}, () => ({
+        id: uuidv4(),
+        title: task,
+      }))
+
+      this.setState(prevState => ({
+        todolist: [...prevState.todolist, ...newTodos],
+        topic: '',
+      }))
+    } else if (topic) {
+      const newTodo = {
+        id: uuidv4(),
+        title: topic,
+      }
+
+      this.setState(prevState => ({
+        todolist: [...prevState.todolist, newTodo],
+        topic: '',
+      }))
+    }
+  }
+
+  /* setLocalStorage = () => {
     const {todolist} = this.state
+    localStorage.setItem('todoList', JSON.stringify(todolist))
+  }
+ */
+
+  render() {
+    const {todolist, topic} = this.state
     return (
       <div className="bg-container">
         <div className="card">
           <h1 className="heading">Simple Todos</h1>
+          <form onSubmit={this.onSubmitForm} className="add-form">
+            <input
+              value={topic}
+              onChange={this.handleTitle}
+              type="text"
+              placeholder="Enter todo title"
+            />
 
+            <button className="add-btn" type="submit">
+              Add
+            </button>
+          </form>
           <ul className="todo-card">
             {todolist.map(each => (
               <TodoItem
+                saveTodo={this.saveTodo}
+                editTodo={this.editTodo}
+                checkTodo={this.checkTodo}
                 functionDelete={this.deleteUser}
                 key={each.id}
                 details={each}
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,17 +1,59 @@
 // Write your code here
+import {useState} from 'react'
 
 import './index.css'
 
 const TodoItem = props => {
-  const {details, functionDelete} = props
-  const {id, title} = details
+  const {details, functionDelete, checkTodo, editTodo, saveTodo} = props
+  const {id, title, isChecked, isEdit} = details
+
+  const [topic, setTopic] = useState(title)
+
   const deleteTodo = () => {
     functionDelete(id)
   }
-  console.log(details.id)
+
+  const handleTopic = e => {
+    setTopic(e.target.value)
+  }
+
   return (
     <li className="list-item">
-      <p className="title">{title}</p>
+      <input
+        id={id}
+        checked={isChecked}
+        onChange={e => checkTodo(e, id)}
+        type="checkbox"
+      />
+      {isEdit ? (
+        <input onChange={handleTopic} type="text" value={topic} />
+      ) : (
+        <p
+          style={{textDecoration: isChecked ? 'line-through' : null}}
+          className="title"
+        >
+          {title}
+        </p>
+      )}
+
+      {isEdit ? (
+        <button
+          onClick={() => saveTodo(id, topic)}
+          className="buttton save"
+          type="button"
+        >
+          Save
+        </button>
+      ) : (
+        <button
+          onClick={() => editTodo(id)}
+          className="buttton edit"
+          type="button"
+        >
+          Edit
+        </button>
+      )}
+
       <button onClick={deleteTodo} className="buttton" type="button">
         Delete
       </button>
